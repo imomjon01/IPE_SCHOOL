4ef0c54454f18801a0ddf45a9072d98d16d674d9
macbook.u <imomjonuzb@gmail.com>
2025-06-09 16:29:55 +0500

    first

---
+++ src/main/java/ipe/school/ipe_school/IpeSchoolApplication.java
@@ -0,0 +1,13 @@
+package ipe.school.ipe_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpeSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpeSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ipe/school/ipe_school/IpeSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ipe.school.ipe_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpeSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
