c540887545615cde5d3f7ab3cc470c1b45c04405
Abdulloh <rahimjonov333@gmail.com>
2025-12-15 11:04:57 +0500

    ishladi

--- src/main/java/ipe/school/ipe_school/security/MySecurityFilter.java
+++ src/main/java/ipe/school/ipe_school/security/MySecurityFilter.java
@@ -24,7 +24,6 @@ public class MySecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader("Authorization");
-        System.out.println(" token : " + token);
         if (token != null && token.startsWith("Bearer ")) {
             token = token.substring(7);
             try {
